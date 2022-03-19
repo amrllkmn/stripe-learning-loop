de4e33efd0dd1fe75114bdef7a4eb0bec2e51fa9
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2022-03-19 23:09:01 +0800

    Add hooks and separate product from main app

--- src/hooks/useProduct.js
+++ src/hooks/useProduct.js
@@ -1,4 +1,7 @@
+import axios from "axios";
 import useSWR from "swr"; // This is a library for data-fetching
+
+// Assumption: Product is stored over a server and is fetched
 
 export default function useProduct() {
     const handleError = (error) => {
@@ -28,4 +31,39 @@ export default function useProduct() {
             }
         }
     )
+
+    const submitItem = async (data) => {
+        console.log(data)
+        axios({
+            method: 'post',
+            url: 'http://localhost:4242/create-checkout-session',
+            headers: { 'Content-Type': 'application/json' },
+            data: {
+               line_items: [
+                   data
+               ]
+            }
+          }).then((response)=> {console.log(response)}).catch((error) => console.error)
+    }
+    return {
+        // return fake data here
+        product: {
+            data:{
+                line_items: [
+                  {
+                    item_name: "Stubborn Attachments",
+                    price_id: 'price_1Kf3J2BlskhJl6mE9F2k7Opu',
+                    price: '20.00'
+                  },
+                  {
+                    item_name: "Stubborn Attachments 2: Electric Bogaloo",
+                    price_id: 'price_1Kf3JoBlskhJl6mEEYuiMUcs',
+                    price: '35.00'
+                  },
+                ],
+              }
+        },
+        error,
+        submitItem
+    }
 }
